6b338adf3d9c4540c87b4fb061639ad9699b8825
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2023-03-01 06:15:31 -0300

    add checkLogin

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -2,19 +2,17 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
 class HomeController extends Controller {
-
-    public function index() {
-        $this->render('home', ['nome' => 'Bonieky']);
-    }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
-    }
-
+	private $loggedUser;
+	public function __construct() {
+		$this->loggedUser = LoginHandler::checkLogin();
+		if(LoginHandler::checkLogin() == false) {
+			$this->redirect('/login');
+		}
+	}
+	public function index() {
+		$this->render('home', ['nome' => 'Jean']);
+	}
 }
