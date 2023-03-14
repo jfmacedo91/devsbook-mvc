044583522623bebb742ee423ee2921bd9dabb930
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2023-03-14 08:57:25 -0300

    404 page styled

--- src/views/pages/404.php
+++ src/views/pages/404.php
@@ -1,1 +1,21 @@
-<h1>Página não encontrada</h1>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pagina não encontrada</title>
+  <style>
+    body {
+      display: grid;
+      place-content: center;
+      min-height: 100vh;
+      font-family: Helvetica;
+      color: #42227a;
+      background-color: #edeef0;
+    }
+  </style>
+</head>
+<body>
+  <h1>Página não encontrada</h1>
+</body>
+</html>
